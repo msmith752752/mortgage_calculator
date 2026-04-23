970ce88b1c0808344745eed82ebcc5d4d3f40406
Matthew R. Smith <155927920+msmith752752@users.noreply.github.com>
2026-04-23 11:56:59 -0400

    Refactor mortgage calculator for clarity and structure
    Update to Python code

--- mortgagecalculator_2.17.py
+++ mortgagecalculator_2.17.py
@@ -1,29 +1,24 @@
-"""Analyze a mortgage loan."""
+"""
+Mortgage Payoff Simulator
+"""
 
 purchase_price = 277_500
 interest_rate = 0.075
 loan_term = 360
 
-# Find a monthly payment that pays off the loan.
 monthly_payment = 1.00
-principal = purchase_price
 
 while True:
-    # Run the life of the loan with the current payment.
-    for payment_num in range(loan_term):
-        interest = (interest_rate/12) * principal
-        toward_principal = monthly_payment - interest
-        principal -= toward_principal
+    principal = purchase_price
 
-    # Summarize results.
-    print(f"Tried {monthly_payment:,.2f}")
-    print(f"  Remaining principal: {principal:,.2f}")
+    for _ in range(loan_term):
+        interest = (interest_rate / 12) * principal
+        principal -= (monthly_payment - interest)
 
-    # Exit loop if loan is paid off.
+    print(f"Tried ${monthly_payment:,.2f}")
+    print(f"Remaining principal: ${principal:,.2f}\n")
+
     if principal <= 0:
         break
 
-    # Loan was not paid off. Increase monthly payment, and reset loan.
     monthly_payment += 1.00
-    principal = purchase_price
-
